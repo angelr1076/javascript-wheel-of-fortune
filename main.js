fc3b1f8e7cce19ccc34ef8ca4e210bc6535424e3
Angel <angelr1076@gmail.com>
2018-10-26 19:22:22 -0400

    refactored code

--- main.js
+++ main.js
@@ -1,39 +1,45 @@
-
-var guessesLeft;
-var letterClicked;
-var wordArray = ["JAVASCRIPT", "ARRAYS", "FUNCTIONS", "HOISTING", "RECURSION", "EVENTS", "KEYUP", "TERNARY"];
-var wordChoice;
-var answerList = [];
-let remainingLetters;
-var newGame;
-
-var hint = document.querySelector(".hint");
-var numbers = document.querySelector("#numbers");
-var guesses = document.querySelector("#guesses");
-var wordDisplay = document.querySelector("#words");
-var letterCount = document.querySelector(".letters");
-var newGame = document.querySelector("#play");
-var letterBoxes = document.querySelector("#alphabet");
-// let letterChoices = document.querySelector("#letterChoices");
-// let letterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "0", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let guessesLeft,
+    letterClicked,
+    wordChoice,
+    remainingLetters,
+    answerList = [],
+    wordArray = ["JAVASCRIPT", "ARRAYS", "FUNCTIONS", "HOISTING", "RECURSION", "EVENTS", "KEYUP", "TERNARY"],
+    hint = document.querySelector(".hint"),
+    letterGuessed = document.querySelector("#your-guess"),
+    numbers = document.querySelector("#numbers"),
+    guesses = document.querySelector("#guesses"),
+    wordDisplay = document.querySelector("#words"),
+    letterCount = document.querySelector(".letters"),
+    newGame = document.querySelector("#play"),
+    letterBoxes = document.querySelector("#alphabet"),
+    titleHeader = document.querySelector(".welcome"),
+    pointTotal = document.querySelector("#pointTotal");
 
 function playGame() {
-    
     guessesLeft = 6;
     guesses.innerHTML = `You have ${guessesLeft} guesses left`;
     // Pick a random word.
     wordChoice = wordArray[Math.floor(Math.random() * wordArray.length)];
 
-    let hintObject = {JAVASCRIPT: "The language of the web", ARRAYS: "Similar to objects", FUNCTIONS: "Stores instructions", HOISTING: "Moving declarations to the top", RECURSION: "Calling functions inside of functions", EVENTS: "This happens to HTML elements", KEYUP: "Not 'onclick', but...", TERNARY: "Type of operator"};
-
-    hint.innerHTML = `Clue: ${hintObject[wordChoice]}`;
-
     for (var i = 0; i < wordChoice.length; i++) {
         answerList[i] = "_";
+        console.log(answerList);
+    }
+    // Display underscores on page representing each letter in the random word
+    wordDisplay.innerHTML = answerList.join('');
+
+    let hintObject = {
+        JAVASCRIPT: "The language of the web",
+        ARRAYS: "Similar to objects",
+        FUNCTIONS: "Stores instructions",
+        HOISTING: "Moving declarations to the top",
+        RECURSION: "Calling functions inside of functions",
+        EVENTS: "This happens to HTML elements; like a concert",
+        KEYUP: "Not 'keydown', but...",
+        TERNARY: "Type of operator"
     }
 
-    // Display underscores on page representing each word in the random word
-    wordDisplay.innerHTML = answerList.join('');
+    hint.innerHTML = `Clue: ${hintObject[wordChoice]}`;
 
     // Display number of letters in the random word on the page
     remainingLetters = wordChoice.length;
@@ -41,24 +47,23 @@ function playGame() {
 }
 
 // Take the player’s guess.
-function buttonPress(e) { 
+function buttonPress(e) {
     letterClicked = e.target.textContent;
-    document.getElementById("your-guess").innerHTML = `You guessed the letter ${letterClicked}`;
+    letterGuessed.innerHTML = `You guessed the letter ${letterClicked}`;
     matchWord(letterClicked);
 }
 
 // Pass the letter event from buttonPress into the randomWord function
 function matchWord(letter) {
-
     if (remainingLetters > 0) {
-        var foundMatch = false;
+        let foundMatch = false;
 
         for (var i = 0; i < wordChoice.length; i++) {
             if (wordChoice[i] === letter) {
-            foundMatch = true;
-            answerList[i] = letter;
-            wordDisplay.innerHTML = answerList.join(' ');
-            remainingLetters--;
+                foundMatch = true;
+                answerList[i] = letter;
+                wordDisplay.innerHTML = answerList.join(' ');
+                remainingLetters--;
             }
         }
 
@@ -66,18 +71,19 @@ function matchWord(letter) {
             guessesLeft--;
             guesses.innerHTML = (`You have ${guessesLeft} guesses left`);
         }
-        
+
         if (guessesLeft === 0) {
-            guesses.innerHTML = "Sorry, you're out of guesses!";
-            setTimeout(function() {
+            hint.innerHTML = "Sorry, you're out of guesses!";
+            setTimeout(function () {
                 hint.innerHTML = "If you'd like to play again, click the spin button.";
-                letterCount.innerHTML = "You lost. You did, you truly did."
+                letterCount.innerHTML = "You lost :(";
             }, 3000);
-        }  
+        }
 
         if (remainingLetters === 0) {
-            letterCount.innerHTML = "Great job! You guessed it!";
-            setTimeout(function() {
+            hint.innerHTML = "Great job! You guessed it!";
+            setTimeout(function () {
+
                 playGame();
             }, 3000);
         }
